4de98300899d58f11475cf1f7f315d46cec2c9de
chenc10 <chenc10@126.com>
2014-06-07 00:20:21 +0800

    6.7

--- src/testfinal/match.sh
+++ src/testfinal/match.sh
@@ -1,13 +1,13 @@
-for i in 1 2 3 4 5 6 7 8 9 10; do
-	cp ./$i[^0-9]*/nids*/*_cTos_tcp ./$i"_cTos_tcp"
-	cp ./$i[^0-9]*/nids*/*_sToc_tcp ./$i"_sToc_tcp"
-	cp ./$i[^0-9]*/nids*/*_cTos_udp ./$i"_cTos_udp"
-	cp ./$i[^0-9]*/nids*/*_sToc_udp ./$i"_sToc_udp"
-done
-cat [0-9]*_cTos_tcp > nids_cTos_tcp
-cat [0-9]*_sToc_tcp > nids_sToc_tcp
-cat [0-9]*_cTos_udp > nids_cTos_udp
-cat [0-9]*_sToc_udp > nids_sToc_udp
+#for i in 1 2 3 4 5 6 7 8 9 10; do
+#	cp ./$i[^0-9]*/nids*/*_cTos_tcp ./$i"_cTos_tcp"
+#	cp ./$i[^0-9]*/nids*/*_sToc_tcp ./$i"_sToc_tcp"
+#	cp ./$i[^0-9]*/nids*/*_cTos_udp ./$i"_cTos_udp"
+#	cp ./$i[^0-9]*/nids*/*_sToc_udp ./$i"_sToc_udp"
+#done
+#cat [0-9]*_cTos_tcp > nids_cTos_tcp
+#cat [0-9]*_sToc_tcp > nids_sToc_tcp
+#cat [0-9]*_cTos_udp > nids_cTos_udp
+#cat [0-9]*_sToc_udp > nids_sToc_udp
 rm -rf matchresult
 rm -rf [0-9]*_*_*
 for i in 1 2 3 4 5 6 7 8 9 10; do
